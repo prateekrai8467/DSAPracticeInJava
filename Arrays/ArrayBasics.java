55abe65e9ef76f3ba377311a8b31f36daa48ffb6
prateekrai8467 <prateekrai175@gmail.com>
2026-06-09 21:21:25 +0530

    Add files via upload

---
+++ Arrays/ArrayBasics.java
@@ -0,0 +1,18 @@
+package Arrays;
+
+public class ArrayBasics {
+    public static void main(String[] args) {
+//        int[] x = {6,19,7,69,9,171,5};
+//        // indexing
+//        System.out.println(x[5]); // access
+//        // updating elements - mutability
+//        x[3] = 89;
+//        System.out.println(x[3]);
+        int[] arr = new int[4]; // 4 size ka array | 0 to 3
+        arr[0] = 10;
+        arr[1] = 20;
+        arr[2] = 150;
+        arr[3] = -8;
+
+    }
+}
